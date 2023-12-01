6c30327bf2fec3fb27d03097f57996708d98a912
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-12-01 14:20:33 +0500

    completed

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-let b = 885;
+let b = 750;
+let c = 120;
 
 let a = +prompt("Bor pulingizni sumasini kiriting");
 
-let sum = a / 11000;
+let sum = b * 11000;
+let sum1 = c * 12354;
 
-if (sum > b) {
-    console.log("Oq yo'l Alisher");
+let sum2 = b + c
+
+let res;
+
+if (a > sum2) {
+    res = ("Oq yo'l Alisher");
 }else{
-    console.log("Alisher ozgina sabir qilish kerak bo'lar ekan");
+    res = ("Alisher ozgina sabir qilish kerak bo'lar ekan");
 }
+
+console.log(res);
