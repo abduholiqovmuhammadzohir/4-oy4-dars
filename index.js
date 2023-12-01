b5dfbbf7cb9c09bf9f83ce3e128afdd884a151b3
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-12-01 14:09:30 +0500

    homework

---
+++ index.js
@@ -0,0 +1,11 @@
+let b = 885;
+
+let a = +prompt("Bor pulingizni sumasini kiriting");
+
+let sum = a / 11000;
+
+if (sum > b) {
+    console.log("Oq yo'l Alisher");
+}else{
+    console.log("Alisher ozgina sabir qilish kerak bo'lar ekan");
+}
